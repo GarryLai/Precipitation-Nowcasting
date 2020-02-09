95d66a3ee28d83fcfdbea2243c4db4e3132a9a20
blt <amyhsiao2015@gmail.com>
2020-02-09 20:31:49 +0800

    Updated file

--- convert_img/convert.py
+++ convert_img/convert.py
@@ -1,16 +1,30 @@
 from PIL import Image
+from datetime import datetime
+from os import listdir
 import numpy as np
+import re
 import switch
 
+color_dict = switch.color_dict
 
-def get_img(file):
-	try:
-		im = Image.open(file).convert("RGB")
-	except FileNotFoundError:
-		print("File not found.")
-		exit(0)
-
-	return im
+def get_files():
+	default_re = ".*.png"
+	while 1:
+		r = re.compile(default_re)
+		l=list(listdir(".\\input_file"))
+		newlist = list(filter(r.match,l)) # Read Note
+		print("file match:")
+		for i in newlist:
+			print(i)
+		confirm = input("comfirm search result?(y/n)")
+		if(confirm == 'y'):
+			break
+		elif(confirm =='n'):
+			print("current expression: "+default_re)
+			default_re = input("please input new regular expression\n")
+		else:
+			print("invalid input")
+	return newlist
 
 def get_boundary(w, h):
 	b = list(map(int, input("crop boundry(l, u, r, d)(separated by space): ").split()))
@@ -26,42 +40,40 @@ def get_boundary(w, h):
 
 	return b
 
-def get_dbz(arr):
+def get_dbz(tup):
  	#arr must be 1d nparray with a size of 3
- 	dbz = switch.color_dict.get(arr.tobytes())
+ 	dbz = color_dict.get(tup)
  	if dbz:
  		return dbz
  	else:
  		return -1
 		
-def grayify(filename, im, w, h):
-	pix = np.array(im)
-	arr = np.zeros([h, w], dtype=np.uint8)
-	l = 0
-	r = 0
+def greyify(filename, im, w, h):
+	pix = im.load()
+	arr = np.zeros([h, w], dtype = np.uint8)
 	for x in range(h):
 		for y in range(w):
-			t = get_dbz(pix[x][y])
+			t = get_dbz(pix[y, x])
 			if t == -1:
-				if get_dbz(pix[x, min(w-1, y+10)]) != -1:
-					arr[x, y:min(w-1, y+10)+1] = 255*(get_dbz(pix[x-1, min(w-1, y+10)])/65)
-				elif get_dbz(pix[min(h-1, x+10), y]) != -1:
-					arr[x:min(h-1, w+10)+1, y] = 255*(get_dbz(pix[min(h-1, x+10), y])/65)
+				if get_dbz(pix[min(w-1, y+10), x]) != -1:
+				 	arr[x, y:min(w-1, y+10)+1] = 255*(1 - get_dbz(pix[min(w-1, y+10), x]))
+				elif get_dbz(pix[y, min(h-1, x+10)]) != -1:
+				  	arr[x:min(h-1, x+10)+1, y] = 255*(1 - get_dbz(pix[y, min(h-1, x+10)]))
 				else:
-					arr[x, y]=0	
+					arr[x, y]= 0	
 			else:
-				arr[x, y] =  255 * (t / 65)
+				arr[x, y] =  255  * (1 - t)
 	newimg = Image.fromarray(arr)
-	# newimg.show()
-	newimg.save(filename+'_greyed.png')
+	newimg.show()
+	newimg.save(".\\output_file\\"+filename[:-4]+'_greyed.png')
 
 if __name__=='__main__':
-	file = input("img name (without .png): ")
-	im = get_img(file+'.png')
-	w, h = im.size
-	l, u, r, d = [0, 0, w, h] #get_boundary(w, h)
-
-	cropped_img = im.crop((l, u, r, d))
-	grayify(file, cropped_img, r - l, d - u)
-	print('Done.')
+	for file in get_files():
+		t1 = datetime.now()
+		im = Image.open(".\\input_file\\"+file)
+		w, h = im.size
+		greyify(file, im, w, h)
+		t2 = datetime.now()
+		print('Done.')
+		print(str(t2-t1)+'\n')
 
